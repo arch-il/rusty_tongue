b1f9ccd7bf91c4a7dfd3ff16950cc56375a6a4fc
arch_il <archilberaia+github@gmail.com>
2025-02-16 23:29:10 +0400

    add simple translation

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,13 @@ use std::{
 };
 
 use console::{Key, Term};
+use rust_translate::translate_to_english;
 
-fn main() {
+#[tokio::main]
+async fn main() {
+    // let english_text = translate_to_english("Hallo, Ich bin Harry.").await.unwrap();
+    // println!("Translated to English: {}", english_text);
+
     let term = Term::stdout();
 
     let file = File::open("book.txt").expect("Failed to read file");
@@ -25,6 +30,8 @@ fn main() {
         }
 
         println!("{}\n", line);
+        let english_line = translate_to_english(&line).await.unwrap();
+        println!("{}\n", english_line);
 
         loop {
             let key = term.read_key();
