2e8c452e6642a3f637e99172c529754212f2ced6
arch_il <archilberaia+github@gmail.com>
2025-02-16 23:33:51 +0400

    test colored text support

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,11 @@ use std::{
     io::{BufRead, BufReader},
 };
 
-use console::{Key, Term};
+use console::{Key, Style, Term};
 use rust_translate::translate_to_english;
 
 #[tokio::main]
 async fn main() {
-    // let english_text = translate_to_english("Hallo, Ich bin Harry.").await.unwrap();
-    // println!("Translated to English: {}", english_text);
-
     let term = Term::stdout();
 
     let file = File::open("book.txt").expect("Failed to read file");
@@ -30,8 +27,10 @@ async fn main() {
         }
 
         println!("{}\n", line);
+
         let english_line = translate_to_english(&line).await.unwrap();
-        println!("{}\n", english_line);
+        let style = Style::new().black().on_white();
+        println!("{}\n", style.apply_to(english_line));
 
         loop {
             let key = term.read_key();
