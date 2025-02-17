62bd08238e91cce24c511ca44521864c4f29177f
arch_il <archilberaia+github@gmail.com>
2025-02-17 18:49:00 +0400

    move draw and input separately

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::{
     fs::File,
-    io::{BufRead, BufReader},
+    io::{BufRead, BufReader, Lines},
 };
 
 use console::{Key, Style, Term};
@@ -12,8 +12,19 @@ async fn main() {
 
     let file = File::open("book.txt").expect("Failed to read file");
     let reader = BufReader::new(file);
+    let mut lines = reader.lines();
 
-    for line in reader.lines() {
+    draw(&mut lines).await;
+    // input(&term);
+}
+
+async fn draw(lines: &mut Lines<BufReader<File>>) {
+    loop {
+        let line = match lines.next() {
+            Some(line) => line,
+            None => continue,
+        };
+
         let line = match line {
             Ok(line) => line,
             Err(e) => {
@@ -32,16 +43,20 @@ async fn main() {
         let style = Style::new().black().on_white();
         println!("{}\n", style.apply_to(english_line));
 
-        loop {
-            let key = term.read_key();
-            match key {
-                Ok(key) => {
-                    if key == Key::ArrowRight {
-                        break;
-                    }
+        break;
+    }
+}
+
+fn input(term: &Term) {
+    loop {
+        let key = term.read_key();
+        match key {
+            Ok(key) => {
+                if key == Key::ArrowRight {
+                    break;
                 }
-                Err(e) => println!("Error while reading a key {e}"),
             }
+            Err(e) => println!("Error while reading a key {e}"),
         }
     }
 }
