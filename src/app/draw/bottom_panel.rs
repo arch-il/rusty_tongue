0dbb28c44291d13d686b01f8fd1bdc82f9437984
arch_il <archilberaia+github@gmail.com>
2025-02-18 23:22:05 +0400

    make location changeable

--- src/app/draw/bottom_panel.rs
+++ src/app/draw/bottom_panel.rs
@@ -1,4 +1,4 @@
-use eframe::egui;
+use eframe::egui::{self, Vec2};
 
 use crate::app::MyEguiApp;
 
@@ -10,7 +10,22 @@ impl MyEguiApp {
                 let max = self.lines.len();
 
                 ui.with_layout(egui::Layout::left_to_right(egui::Align::Center), |ui| {
-                    ui.label(format!("loc: {} / {}", pos, max));
+                    ui.spacing_mut().item_spacing = Vec2::new(3.0, 0.0);
+
+                    ui.label("loc:");
+
+                    let mut search_text = (self.index + 1).to_string();
+                    let text_edit = egui::TextEdit::singleline(&mut search_text)
+                        .clip_text(false)
+                        .desired_width(0.0);
+                    ui.add(text_edit).clicked();
+                    let temp = search_text.parse().unwrap_or(self.index) - 1;
+                    if temp != self.index {
+                        self.index = temp;
+                        self.get_history_entry();
+                    }
+
+                    ui.label(format!("/ {max}"));
                 });
 
                 ui.with_layout(egui::Layout::right_to_left(egui::Align::Center), |ui| {
